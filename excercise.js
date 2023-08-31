0798cf60e827608debe494ac5915985dc993acc1
dimassagngsptr <dimasageng631@gmail.com>
2023-08-31 09:45:02 +0700

    first commit

---
+++ excercise.js
@@ -0,0 +1,81 @@
+// membuat variabel untuk menyimpan panjang dan lebar persegi
+// p = panjang
+// l = lebar
+// let panjang = 10;
+// let lebar = 5;
+
+// // menghitung luas persegi panjang
+// const luas = panjang * lebar;
+// console.log(luas);
+
+// // menghitung keliling persegi panjang
+// let k = panjang + lebar;
+// const keliling = 2 * k;
+// console.log(keliling);
+
+// // jari jari lingkaran
+// let jariJari = 4;
+// // menghitung diameter lingkaran
+// const diameterLingkaran = 2 * jariJari;
+// console.log(diameterLingkaran);
+
+// // menghitung keliling lingkaran
+// const kelilingLingkaran = 3.14 * diameterLingkaran;
+// console.log(kelilingLingkaran);
+
+// // menghitung luas lingkaran
+// const luasLingkaran = 3.14 * jariJari ** 2;
+// console.log(luasLingkaran);
+
+// // segitiga sama kaki
+// // memiliki 3 sudut diantara lain
+// // A = 70
+// // B= 70
+// // C = ?
+
+// // rumus menemukan sudut segitiga
+// // menghitung sudut BAC
+// let s1 = 70;
+// let s2 = 70;
+
+// let s3 = 180 - (s1 - s2);
+// console.log(s3);
+
+// // mendapatkan data hari ini
+// const date1 = new Date();
+// console.log(date1);
+
+// // mendapatkan data tahun baru
+// const date2 = new Date("2024-01-01");
+// console.log(date2);
+
+// // menghitung jumlah selisih date1 dan date2
+// let selisih = date2.getTime() - date1.getTime();
+
+// // jumlah hari menuju tahun baru
+// let days = Math.floor(selisih / (24 * 60 * 60 * 1000));
+// console.log(`jumlah hari menuju tahun baru adalah ${days} hari`);
+
+// // menghitung tahun
+// let tahun = Math.floor(days / 365);
+// let sisa = days % 365;
+// let bulan = Math.floor(sisa / 30);
+// let hari = sisa % 30;
+// console.log(` ${bulan} bulan, ${tahun} tahun`);
+
+// for (let i = 0 ; i <= 10; i++) {
+//     console.log (i);
+// }
+
+// let person = {
+//    name: "John",
+//    age: 34,
+// };
+// let person2 = {
+//    name: "John",
+//    age: 34,
+// };
+
+// console.log(person.age == person2.age);
+
+
---
+++ main.js
@@ -0,0 +1,48 @@
+// ini komen pertama
+// console.log("Hello world!");
+
+// Ini komen
+// 2 baris
+
+// // membuat variable
+// let message;
+
+// // menambahkan data kedalam variable
+// message = "Hello!";
+
+// // menampilakan data di dalam variable
+// console.log(message);
+
+// var globaVariable = "Global variables";
+
+// let replacable = "This value will be replaced";
+// replacable = "I love javascript";
+
+// const constant = "Hello Purwadhika";
+
+// const message = "javascript";
+// const number = 1;
+// const bigInt = 212379812738124982149812n;
+// const isTrue = true;
+// const isFalse = false;
+// const noData = null;
+// let noAssigned;
+// const person = {
+//    name: "Joni",
+//    age: 20,
+// };
+
+// console.log(typeof message);
+// console.log(typeof number);
+// console.log(typeof bigInt);
+// console.log(typeof isTrue);
+// console.log(typeof isFalse);
+// console.log(typeof noData);
+// console.log(typeof noAssigned);
+// console.log(typeof person);
+
+// let str = "purwadhika";
+// console.log(`${str} bandung`);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
