86886f70ecd4311d1477f21e0402df4bb833ca5b
dimassagngsptr <dimasageng631@gmail.com>
2023-09-02 21:14:13 +0700

    remove test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-let str = ["Purwadhika", "Bandung", "Jakarta", " Batam"];
-let strJoin = "";
-// console.log(strJoin);
-
-for (let i = 0; i < str.length; i++) {
-   strJoin += str[i];
-   
-}
-console.log(strJoin);
