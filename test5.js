09a5196252d534fcad40b46c7e2900447c8a927b
dimassagngsptr <dimasageng631@gmail.com>
2023-09-07 21:10:22 +0700

    day-8

--- test5.js
+++
@@ -1,2 +0,0 @@
-let factorial = (number) => (number == 0 ? 1 : number * factorial(number - 1));
-console.log(factorial(5));
---
+++ testing2.js
@@ -0,0 +1,6 @@
+// let name = (n) => {
+//     return n
+// }
+// // console.log(name("Dimas"))
+// let name1 = name("Dimas")
+// console.log(name1)
